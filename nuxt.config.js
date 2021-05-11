d1c49c07c67837e19bf7c3f0a2c7cb954f0c37a1
Louis Sandoval <louis@sandoval.family>
2021-05-11 00:04:39 -0700

    Add website description to Nuxt and NPM

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,39 +1,43 @@
 export default {
-  // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+	// Target: https://go.nuxtjs.dev/config-target
+	target: "static",
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'Brisket',
-    htmlAttrs: {
-      lang: 'en',
-    },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-  },
+	// Global page headers: https://go.nuxtjs.dev/config-head
+	head: {
+		title: "Brisket",
+		htmlAttrs: {
+			lang: "en",
+		},
+		meta: [
+			{ charset: "utf-8" },
+			{ name: "viewport", content: "width=device-width, initial-scale=1" },
+			{
+				hid: "description",
+				name: "description",
+				content: "A delicious Tezos interface for the Trezor hardware wallet.",
+			},
+		],
+		link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+	},
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+	// Global CSS: https://go.nuxtjs.dev/config-css
+	css: [],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+	plugins: [],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+	// Auto import components: https://go.nuxtjs.dev/config-components
+	components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
-  ],
+	// Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+	buildModules: [
+		// https://go.nuxtjs.dev/typescript
+		"@nuxt/typescript-build",
+	],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+	// Modules: https://go.nuxtjs.dev/config-modules
+	modules: [],
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
-}
+	// Build Configuration: https://go.nuxtjs.dev/config-build
+	build: {},
+};
