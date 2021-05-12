5421205ddf6666c02ee611b89a499bc296ce2461
Louis Sandoval <louis@sandoval.family>
2021-05-12 16:40:04 -0700

    Change favicon to svg cut of meat
    - Also add OG Image meta tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,8 +16,18 @@ export default {
 				name: "description",
 				content: "A delicious Tezos interface for the Trezor hardware wallet.",
 			},
+			{
+				property: "og:image",
+				content: "/imgs/cut_of_meat.png",
+			},
 		],
-		link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+		link: [
+			{
+				rel: "icon",
+				type: "image/svg+xml",
+				href: "data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20100%20100%22%3E%3Ctext%20y%3D%22.9em%22%20font-size%3D%2290%22%3E%F0%9F%A5%A9%3C%2Ftext%3E%3C%2Fsvg%3E",
+			},
+		],
 	},
 
 	// Global CSS: https://go.nuxtjs.dev/config-css
