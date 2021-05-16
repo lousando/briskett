8a86a939e264d2115724958a2100c533f3e66d30
Louis Sandoval <louis@sandoval.family>
2021-05-16 14:53:38 -0700

    Configure Jest test suite

---
+++ jest.config.ts
@@ -0,0 +1,23 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/en/configuration.html
+ */
+
+export default {
+	moduleNameMapper: {
+		"^@/(.*)$": "<rootDir>/$1",
+		"^~/(.*)$": "<rootDir>/$1",
+		"^vue$": "vue/dist/vue.common.js",
+	},
+	moduleFileExtensions: ["ts", "js", "vue", "json"],
+	transform: {
+		"^.+\\.ts$": "ts-jest",
+		"^.+\\.js$": "babel-jest",
+		".*\\.(vue)$": "vue-jest",
+	},
+	collectCoverage: true,
+	collectCoverageFrom: [
+		"<rootDir>/components/**/*.vue",
+		"<rootDir>/pages/**/*.vue",
+	],
+};
